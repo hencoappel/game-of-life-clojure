4f94037ba5fa6e52856070f19608a7a15dce4de7
Henco Appel <hencoappel@gmail.com>
2015-07-10 17:15:55 +0100

    Got a grid showing!

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -1,6 +1,7 @@
 (ns ^:figwheel-always om-tut.core
-    (:require[om.core :as om :include-macros true]
-              [om.dom :as dom :include-macros true]))
+    (:require [om.core :as om :include-macros true]
+              [om.dom :as dom :include-macros true]
+              [om-tut.grids :as grids]))
 
 (enable-console-print!)
 
@@ -8,13 +9,30 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world! This is Henco."}))
+(defonce app-state (atom {:squares grids/gosper-glider-gun}))
+
+(defn gen-grid-style
+  [data]
+  (let [sqr-size 10
+        width (str (* sqr-size (count (get data 0))) "px")
+        height (str (* sqr-size (count data)) "px")]
+    #js {:width width :height height}))
+
+; (defn set-interval [callback ms]
+;   (future (while true (do (Thread/sleep ms) (callback)))))
+
+; (def job (set-interval update grid 1000))
+
+; (future-cancel job)
+
+; (defn timer [] (java.util.Timer.))
+; (doseq (range 1000)
 
 (om/root
   (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text data)))))
+    (om/component
+      (apply dom/div #js {:style (gen-grid-style (:squares data))}
+        (->> (:squares data) (flatten) (map #(get ["dead" "live"] %)) (map (fn [s] (dom/div #js {:className (str "square " s)} nil)))))))
   app-state
   {:target (. js/document (getElementById "app"))})
 
